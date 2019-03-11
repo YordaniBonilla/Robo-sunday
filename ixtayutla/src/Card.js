1ace264a01678a5f43a77b0631b89daf58555b53
YordaniBonilla <b.yordani93@gmail.com>
2019-03-10 18:10:26 -0600

    es6 destructoring

--- ixtayutla/src/Card.js
+++ ixtayutla/src/Card.js
@@ -1,17 +1,16 @@
 import React from 'react';
 //import our react library
 //alt is used incase original src doesnt load
-const Card = (props) => {
+const Card = ({ name, email, username, id }) => {
 	//in order to no repeat more lines
-	const { name, email, username, id } = props;
 	return (
 		<div p title="its alive!" className="bg-dark-blue dib br3 pa3 ma2 grow dim bw2 shadow-5 ">
-			<img alt="robots" src={`https://www.robohash.org/${props.id}?200x200`} />
+			<img alt="robots" src={`https://www.robohash.org/${id}?200x200`} />
 			<div>
-				<h2>{props.name}</h2>
-				<p>{props.email}</p>
-				<p>{props.username}</p>
-				<p>{props.id}</p>
+				<h2>{name}</h2>
+				<p>{email}</p>
+				<p>{username}</p>
+				<p>{id}</p>
 			</div>
 		</div>
 	);
